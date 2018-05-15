ec882102f67c3c596009c27a72cb0572fad8c132
rikutons <rikutonsicloud@gmail.com>
2018-05-15 21:15:09 +0900

    Tentative commit for checkout to develop

--- Program/move_input.cpp
+++ Program/move_input.cpp
@@ -3,6 +3,11 @@
 #include "move.hpp"
 #include "keyboard.hpp"
 
+void SetDir(Move *m, DIRECTION value) {
+	m->dir = value;
+	if (m->action == NONE) m->action = MOVE;
+}
+
 void moveInput(Move moves[]) {
 	for (int i = 0; i < 2; i++) {
 		//エージェント1
@@ -31,4 +36,49 @@ void moveInput(Move moves[]) {
 				moves[i * 2 + 1].action != MOVE ? MOVE : ERASE);
 		}
 	}
+	/*for (int i = 1; i < 2; i++) {
+		//エージェント1
+		if (keyboardGet(KEY_INPUT_UP) > 0)
+			SetDir(&moves[i * 2], U);
+		else if (keyboardGet(KEY_INPUT_PGUP) > 0)
+			SetDir(&moves[i * 2], RU);
+		else if (keyboardGet(KEY_INPUT_RIGHT) > 0)
+			SetDir(&moves[i * 2], R);
+		else if (keyboardGet(KEY_INPUT_PGDN) > 0)
+			SetDir(&moves[i * 2], RD);
+		else if (keyboardGet(KEY_INPUT_DOWN) > 0)
+			SetDir(&moves[i * 2], D);
+		else if (keyboardGet(KEY_INPUT_END) > 0)
+			SetDir(&moves[i * 2], DL);
+		else if (keyboardGet(KEY_INPUT_LEFT) > 0)
+			SetDir(&moves[i * 2], L);
+		else if (keyboardGet(KEY_INPUT_HOME) > 0)
+			SetDir(&moves[i * 2], LU);
+
+		if (keyboardGet(KEY_INPUT_INSERT) == 1) {
+			moves[i * 2].action = (moves[i * 2].action != MOVE ? MOVE : ERASE);
+		}
+		//エージェント2
+		if (keyboardGet(KEY_INPUT_NUMPAD8) > 0)
+			SetDir(&moves[i * 2 + 1], U);
+		else if (keyboardGet(KEY_INPUT_NUMPAD9) > 0)
+			SetDir(&moves[i * 2 + 1], RU);
+		else if (keyboardGet(KEY_INPUT_NUMPAD6) > 0)
+			SetDir(&moves[i * 2 + 1], R);
+		else if (keyboardGet(KEY_INPUT_NUMPAD3) > 0)
+			SetDir(&moves[i * 2 + 1], RD);
+		else if (keyboardGet(KEY_INPUT_NUMPAD2) > 0)
+			SetDir(&moves[i * 2 + 1], D);
+		else if (keyboardGet(KEY_INPUT_NUMPAD1) > 0)
+			SetDir(&moves[i * 2 + 1], DL);
+		else if (keyboardGet(KEY_INPUT_NUMPAD4) > 0)
+			SetDir(&moves[i * 2 + 1], L);
+		else if (keyboardGet(KEY_INPUT_NUMPAD8) > 0)
+			SetDir(&moves[i * 2 + 1],LU);
+
+		if (keyboardGet(KEY_INPUT_NUMPAD0) == 1) {
+			moves[i * 2 + 1].action = (moves[i * 2 + 1].action != MOVE ? MOVE : ERASE);
+		}
+	}*/
+
 }
